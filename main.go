14b32588190b13b9884769fb84290035e13a029f
Federico Salvetti <salvettifederico68@gmail.com>
2023-06-07 10:21:33 +0200

    commit 2

--- main.go
+++ main.go
@@ -2,16 +2,16 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
-	"prova"
 )
 
 func main() {
 
 	listenAddr := flag.String("listenaddr", ":3000", "the server address")
+	flag.Parse()
 
 	server := api.NewServer(*listenAddr)
+	fmt.Println("server running on port: ", *listenAddr)
 	log.Fatal(server.Start())
-
-	flag.Parse()
 }
